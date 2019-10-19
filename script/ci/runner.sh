f8a61cf61a5ece9533f754d3103c27bf30ae8d34
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2019-10-19 21:47:39 +0300

    Revert "TravisCI: Testing Configuration"
    This reverts commit d36ba04296241b91f80dcf09db4cce762789d72b.

--- script/ci/runner.sh
+++ script/ci/runner.sh
@@ -38,18 +38,27 @@
 
 set -e
 
-#run "chmod 777 frontend/scripts/link_plugin_placeholder.js"
+run "chmod 777 frontend/scripts/link_plugin_placeholder.js"
 
-# if [ "$4" = "bim" ]; then
-#   export OPENPROJECT_EDITION="$4";
-# else
-#   unset OPENPROJECT_EDITION
-# fi
+if [ "$4" = "bim" ]; then
+  export OPENPROJECT_EDITION="$4";
+else
+  unset OPENPROJECT_EDITION
+fi
 
 # Use the current HEAD as input to the seed
-# export CI_SEED=$(git rev-parse HEAD | tr -d 'a-z' | cut -b 1-5 | tr -d '0')
+export CI_SEED=$(git rev-parse HEAD | tr -d 'a-z' | cut -b 1-5 | tr -d '0')
 # Do not assume to have the angular cli running to serve assets. They are provided
 # by rails assets:precompile
-# export OPENPROJECT_CLI_PROXY=''
+export OPENPROJECT_CLI_PROXY=''
 
-bundle exec rspec spec/models/user_spec.rb
+case "$1" in
+        npm)
+            cd frontend && npm run test
+            ;;
+        plugins:cucumber)
+            bundle exec rake parallel:$1 -- --group-number $2 --only-group $3
+            ;;
+        *)
+            bundle exec rake parallel:$1 -- --group-number $2 --only-group $3 --seed $CI_SEED
+esac
--- script/ci/setup.sh
+++ script/ci/setup.sh
@@ -52,12 +52,14 @@ else
 fi
 
 # run migrations for mysql or postgres
-run "bundle exec rake db:migrate"
+if [ $1 != 'npm' ]; then
+  run "bundle exec rake db:migrate"
+fi
 
-# if [ $1 = 'npm' ]; then
-#   run "for i in {1..3}; do npm install && break || sleep 15; done"
-#   echo "No asset compilation required"
-# fi
+if [ $1 = 'npm' ]; then
+  run "for i in {1..3}; do npm install && break || sleep 15; done"
+  echo "No asset compilation required"
+fi
 
 if [ $1 = 'units' ]; then
   # Install pandoc for testing textile migration
@@ -65,8 +67,13 @@ if [ $1 = 'units' ]; then
   run "sudo apt-get install -qq pandoc"
 fi
 
+if [ ! -f "public/assets/frontend_assets.manifest.json" ]; then
+  if [ -z "${RECOMPILE_ON_TRAVIS_CACHE_ERROR}" ]; then
+    echo "ERROR: asset manifest was not properly cached. exiting"
+    exit 1
+  else
+    run "bash $(dirname $0)/cache_prepare.sh"
+  fi
+fi
 
-run "bash $(dirname $0)/cache_prepare.sh"
-
-
-#run "cp -rp public/assets/frontend_assets.manifest.json config/frontend_assets.manifest.json"
+run "cp -rp public/assets/frontend_assets.manifest.json config/frontend_assets.manifest.json"
